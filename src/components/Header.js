8f06f2e4a0524592fc16929aacef10484185783d
jromest <jromestiller@gmail.com>
2018-11-09 13:19:38 +0800

    feat:header uses components styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,18 @@
 import React from "react";
 
+import style from "../styles/components/header.module.scss";
+
 const Header = props => (
-  <header style={tempStyle.header}>
-    <div style={tempStyle.container}>
-      <div style={tempStyle.title}>{props.title}</div>
-      <nav style={tempStyle.nav}>
-        <li style={tempStyle.navItem}>Home</li>
-        <li style={tempStyle.navItem}>About</li>
-        <li style={tempStyle.navItem}>Contact</li>
+  <header className={style.header}>
+    <div className={style.container}>
+      <div className={style.title}>{props.title}</div>
+      <nav className={style.nav}>
+        <li className={style.navItem}>Home</li>
+        <li className={style.navItem}>About</li>
+        <li className={style.navItem}>Contact</li>
       </nav>
     </div>
   </header>
 );
-
-const tempStyle = {
-  header: {
-    backgroundColor: "#c5c5c5"
-  },
-  container: {
-    margin: "0 auto",
-    maxWidth: "1200px",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "20px 0"
-  },
-  title: {
-    fontSize: "2rem",
-    fontWeight: "bold"
-  },
-  nav: {
-    listStyle: "none"
-  },
-  navItem: {
-    display: "inline-block",
-    marginLeft: "20px"
-  }
-};
 
 export default Header;
