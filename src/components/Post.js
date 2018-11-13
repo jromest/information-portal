889974fa2a38a7a3d5e91ce447c8b472342b829c
jromest <jromestiller@gmail.com>
2018-11-13 18:09:03 +0800

    feat: post list component

---
+++ src/components/Post.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Post = ({ post }) => {
+  const { message, name, date, likes, comments } = post;
+  return (
+    <div>
+      <h2>{message}</h2>
+      <h6>{name}</h6>
+      <p>{date}</p>
+      <p>{likes} likes</p>
+      {comments.length === 0 ? (
+        <p>0 comments</p>
+      ) : (
+        comments.map((comment, index) => <p key={index}>{comment}</p>)
+      )}
+    </div>
+  );
+};
+
+export default Post;
