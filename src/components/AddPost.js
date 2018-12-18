77b5048d38f4c73635afd9a551f9dacfd8301ed0
jromest <jromestiller@gmail.com>
2018-12-18 18:11:33 +0800

    feat: style buttons

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -3,8 +3,8 @@ import React from "react";
 const AddPost = ({ onChange, onClick, hasNoPost }) => (
   <div className="form-container">
     <textarea onChange={onChange} />
-    <button onClick={onClick} disabled={hasNoPost}>
-      Add
+    <button className="btn btn-primary" onClick={onClick} disabled={hasNoPost}>
+      Post
     </button>
   </div>
 );
