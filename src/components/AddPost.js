cbea874b27f32e1ad155eaedd93ff45e92743ec3
jromest <jromestiller@gmail.com>
2018-12-21 09:12:37 +0800

    feat: style add post form

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const AddPost = ({ onChange, onClick, hasNoPost }) => (
-  <div className="form-container">
-    <textarea onChange={onChange} />
+  <div className="post-form-container">
+    <textarea className="post-input" rows="5" onChange={onChange} />
     <button className="btn btn-primary" onClick={onClick} disabled={hasNoPost}>
       Post
     </button>
