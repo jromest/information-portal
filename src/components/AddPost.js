7cf3cae2c868b61aab9b3959744509006f881c7f
Jrom <jromestiller@gmail.com>
2019-01-08 14:36:41 +0800

    feat: create global styles for input fields and textarea

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AddPost = ({ onChange, onClick, hasNoPost }) => (
   <div className="post-form-container">
-    <textarea className="post-input" rows="5" onChange={onChange} />
+    <textarea className="text-input" rows="5" onChange={onChange} />
     <button className="btn btn-primary" onClick={onClick} disabled={hasNoPost}>
       Post
     </button>
