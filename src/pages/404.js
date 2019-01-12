766df94ee11a830ea6d0a839b3567a1de2e61fc9
Jrom <jromestiller@gmail.com>
2019-01-13 00:52:54 +0800

    feat: initial 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const NotFound = () => (
-  <div>
-    <h1>404</h1>
-    <h2>Not Found</h2>
+  <div className="notfound-container u-text-center">
+    <h1>404 &bull; Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
 );
 
